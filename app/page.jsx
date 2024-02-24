9efdb9720d65455c86616e37646a3ee0476d397b
Cristian Zambrano <criszamcham@gmail.com>
2024-02-23 18:39:57 -0700

    Add mobile nav

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import Feed from '@components/Feed';
 
 const Home = () => {
   return (
-    <section className="w-full flex-center-flex-col">
-        <h1 className="head_text text_centerx">
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
             Discover & Share
             <br className="max-md:hidden" />
             <span className="orange_gradient text-center">
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 const Nav = () => {
   const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setProviders = async () => {
@@ -78,7 +79,35 @@ const Nav = () => {
               width={37} 
               height={37}
               className='rounded-full'
+              onClick={() => setToggleDropdown((prev) => !prev)}
             />
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <Link href='/profile'
+                className='dropdown_link'
+                onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link href='/profile'
+                className='dropdown_link'
+                onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                type='button'
+                onClick={() => {
+                  setToggleDropdown(false);
+                  signOut();
+                }}
+                className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+
+              </div>
+            )}
           </div>
 
         ) : 
