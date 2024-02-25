22a82d5b38368d00752f560377cb112a6b5a3265
Cristian Zambrano <criszamcham@gmail.com>
2024-02-25 00:08:31 -0700

    Update session

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,7 +11,9 @@ const handler = NextAuth({
         })
     ],
     async session({session}) {
-
+        const sessionUser = await User.findOne({email: session.user.email});
+        session.user.id = sessionUser._id.toString();
+        return session;
     },
     async signIn ({profile}) 
     {
