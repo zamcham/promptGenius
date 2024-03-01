6c66c56a582dbb998769ea4951c9106f0be18116
Cristian Zambrano <criszamcham@gmail.com>
2024-03-01 16:30:42 -0500

    Update form set up

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+'use client';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { userRouter } from 'next/navigation';
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
+    const [submitting, setSubmitting] = useState(false);
+
+    const [post, setPost] = useState({
+        prompt: '',
+        tag:'',
+    });
+
+    const createPrompt = async (e) => {}
+
   return (
-    <div>CreatePrompt</div>
+    <Form
+        type='Create'
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={createPrompt}
+    />
   )
 }
 
