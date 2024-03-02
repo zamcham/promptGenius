4db75b795e80abcfc35dda2e6351ae183ea50a3c
Cristian Zambrano <criszamcham@gmail.com>
2024-03-02 16:13:45 -0500

    Created prompt model

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,5 +1,13 @@
 import { connectToDB } from '@utils/database';
+import Prompt from '@models/prompt';
 
 export const POST = async (req) => {
     const { prompt, userId, tag } = req.json();
+
+    try {
+        await connectToDB();
+
+    } catch (error) {
+        
+    }
 }
