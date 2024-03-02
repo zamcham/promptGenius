5f06fad94af959baa50db4430a5299a4be4acf6f
Cristian Zambrano <criszamcham@gmail.com>
2024-03-02 11:37:07 -0500

    Add tag input

--- components/Form.jsx
+++ components/Form.jsx
@@ -25,6 +25,19 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             className='form_textarea'
           />
         </label>
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Tag{' '}
+            <span className='font-normal'>(#product, #webdev, #idea)</span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value}) }
+            placeholder='#tag'
+            required
+            className='form_input'
+          />
+        </label>
       </form>
     </section>
   )
