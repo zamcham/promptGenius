4a57e3b67bd942090df3c0eb8e8ddb8eb5e8f6f6
Cristian Zambrano <criszamcham@gmail.com>
2024-03-02 11:30:21 -0500

    Create text area

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import Link from 'next/link';
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>Form</div>
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span>
+      </h1>
+      <p className='desc text-left max-w-md'>
+        {type} and share amazing prompts with the world and let your imagination run wild with any AI powered platform.
+      </p>
+      <form 
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Your AI prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value}) }
+            placeholder='Write your prompt here...'
+            required
+            className='form_textarea'
+          />
+        </label>
+      </form>
+    </section>
   )
 }
 
