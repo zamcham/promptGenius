746ec274c6219c49ce087595cf3be76074d4efe5
Cristian Zambrano <criszamcham@gmail.com>
2024-03-02 15:56:49 -0500

    Update form components

--- components/Form.jsx
+++ components/Form.jsx
@@ -38,6 +38,18 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             className='form_input'
           />
         </label>
+        <div className='flex-end mx-3 mb-5 gap-4'>
+          <Link href='/' className='text-gray-500 text-sm'>
+            Cancel
+          </Link>
+          <button
+            type='submit'
+            disabled={submitting}
+            className='px-5 py-1 text-sm bg-primary-orange rounded-full text-white'
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   )
