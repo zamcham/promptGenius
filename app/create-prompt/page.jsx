195644ff0cb55e3787361bc225ddbd5362b63ae1
Cristian Zambrano <criszamcham@gmail.com>
2024-03-02 16:25:36 -0500

    Add missing const

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,43 +1,45 @@
 'use client';
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { userRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
-    const [submitting, setSubmitting] = useState(false);
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
 
-    const [post, setPost] = useState({
-        prompt: '',
-        tag:'',
-    });
+  const [post, setPost] = useState({
+      prompt: '',
+      tag:'',
+  });
 
-    const createPrompt = async (e) => {
-      e.preventDefault();
-      setSubmitting(true);
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
-      try {
-        const response = await fetch('/api/prompt/new', 
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            prompt: post.prompt,
-            userId: session?.user.id,
-            tag: post.tag,
-          })
+    try {
+      const response = await fetch('/api/prompt/new', 
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
         })
+      })
 
-        if (response.ok)
-        {
-          router.push('/');
-        }
-
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setSubmitting(false);
+      if (response.ok)
+      {
+        router.push('/');
       }
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
     }
+  }
 
   return (
     <Form
